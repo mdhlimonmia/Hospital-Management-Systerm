f3524a2160db7c09c6a45ca122ec1329a3328c6d
mdhlimonmia <mdhlimonmia@google.com>
2023-12-16 00:38:20 +0600

    Hospital Management System

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "\n\t\t\t\t............Welcome To TechMed Solutions Hospital............\n";
+
+    string n = "1";
+    while (n[0] - '0')
+    {
+        cout << "\n1.Admission.  "
+             << "2.Doctor List.  "
+             << "3.Patient Details.  "
+             << "4.Payment.  "
+             << "5.Patient_Release.  "
+             << "6.Update Patient info.  "
+             << "7.Authority Dashboard.  "
+             << "8.About.  "
+             << "0.Exit.\n\n";
+        cout << "Enter Your Choice:  ";
+        cin >> n;
+        switch (n[0])
+        {
+        case '1':
+        {
+        }
+        }
+    }
+    cout << "\n\t\t\t\t...........Thanks. Wishing you good health...........\n";
+}
